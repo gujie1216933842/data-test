ed5fbd95d207325ba4044e5d8a6133bfad5b5a05
gujie1216933842 <gujientsy@163.com>
2018-03-13 23:27:28 +0800

    ''

--- data.py
+++ data.py
@@ -30,11 +30,46 @@ import pandas as pda
 
 '''
 pandas中两种类型数据
-Series:   表格中的一行或是一列
-DataFrame:数据框(类似表格)
+Series:   表格中的一行或是一列  对应的是一维数组
+DataFrame:数据框(类似表格)  对应的是二维数组
 '''
 d = pda.Series([2, 9, 4])
 print(d)  # index索引也会打印出来
 # 要指定缩影
-d_index = pda.Series([2, 9, 4],index=[3,4,4] )
+d_index = pda.Series([2, 9, 4], index=[3, 4, 4])
 print(d_index)
+
+# 数据框类型数据创建
+e = pda.DataFrame([[1, 3, 4], [3, 2, 5], [6, 4, 8]])
+print(e)
+
+# 指定数据框的列名
+e_column = pda.DataFrame([[1, 3, 4], [3, 2, 5], [6, 4, 8]], columns=["one", "two", "three"])
+print(e_column)
+
+# 以字典形式创建数据框 , 字典中的key 值数据框中的列名
+e_dict = pda.DataFrame({
+    "one": 4,
+    "two": [2, 5, 3],
+    "three": [5, 7, 5]
+})
+print(e_dict)
+
+
+#调用数据框的头部数据(默认显示的是前五行)
+e_head = e_dict.head(2)
+print(e_head)
+
+#取数据框的尾部数据
+e_tail = e_dict.tail(1)
+print(e_tail)
+
+#统计数据框的行数
+# count:元素格式   mean:平均数   std:标准差  min/max:这一列中的最小/大值   25% : 对应的代表每一列的分位数(前,中,后)
+e_desc = e_dict.describe()
+print(e_desc)
+
+
+#转置  (把行变成列,列变成行)
+e_t = e_dict.T
+print(e_t)
