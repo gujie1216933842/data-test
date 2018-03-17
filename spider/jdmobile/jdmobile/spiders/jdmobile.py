745b34284957df61e48c689bb77cd69b549df36b
gujie1216933842 <gujientsy@163.com>
2018-03-17 11:35:06 +0800

    spider

---
+++ spider/jdmobile/jdmobile/spiders/jdmobile.py
@@ -0,0 +1,16 @@
+#Author:Bob
+import scrapy
+from jdmobile.items import JdmobileItem
+
+class jdmobile:
+    name = "jdmobile"
+    allowed_domains = ["jd.com"]
+    start_urls = (
+        "https://item.jd.com/11334636448.html"
+    )
+
+    def parse(self,response):
+        item = JdmobileItem()
+        #提取手机,xpath表达式
+        item['title'] = response.xpath("//meta[@name='keywords']/@content").extract()
+        print(item['title'])
