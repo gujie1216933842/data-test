045d440c089a98caae71e32a6730d12692f73609
gujie1216933842 <gujientsy@163.com>
2018-03-25 00:35:24 +0800

    pandas数据分析

--- insert_data.py
+++ insert_data.py
@@ -4,7 +4,12 @@
 #导入csv数据
 import pandas as pda
 i=pda.read_csv("C:/Users/Administrator/Desktop/dd.csv",encoding = 'gb18030') #如果encoding参数不加,可能会报错
-print(i.describe())
+#print(i.describe())
 
+#根据某一列排序数据
+i_sort = i.sort_values(by='price')
+#i_comment_count = i.sort_values(by="comment_count")
+print(i_sort)
+#print(i_comment_count)
 
 
